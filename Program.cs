309db16a6669f9e1139e0e8bc3572f250faa14ec
ViscountFish <waldencifuentes85@gmail.com>
2024-08-28 10:12:43 -0500

    finshed version

--- Program.cs
+++ Program.cs
@@ -10,6 +10,36 @@ namespace Random_Number_Game
     {
         static void Main(string[] args)
         {
+            bool keepPlaying = true; 
+
+            do
+            {
+                Random randomNumberGenerator = new Random();
+                int realnumber = randomNumberGenerator.Next(1, 101); //1 - (101 -1)
+
+                Console.Write("Please guess a number between 0 and 100: ");
+                int guess = Convert.ToInt32(Console.ReadLine());
+                int amountguesses = 1;
+                while (guess != realnumber)
+                {
+                    amountguesses++;
+                    Console.Write("You guessed wrong, try guessing {0}; ", guess < realnumber ? "higher" : "lower");
+                    guess = Convert.ToInt32(Console.ReadLine());
+                }
+
+                Console.WriteLine(Environment.NewLine + "You guessed right, it took you {0} attempts.", amountguesses);
+                Console.Write(Environment.NewLine + "Do want to keep playing? (y/n): ");
+                string playOption = Console.ReadLine();
+
+                if (playOption.ToLower() == "n")
+                keepPlaying = false;
+
+            } while (keepPlaying);
+
+      
+            Console.WriteLine("Thank you for playing this game.");
+
+            Console.ReadLine();
         }
     }
 }
